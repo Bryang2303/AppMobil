37a3c6d5c8353c66d831ee39756f80c540b9dc3a
Bryang2303 <bryang2303@gmail.com>
2020-12-16 21:45:01 -0600

    Ventanas Diseño de primera y segunda ventana, trancisiones.

---
+++ app/src/main/java/com/example/linked_listxor/InputActivity.kt
@@ -0,0 +1,17 @@
+package com.example.linked_listxor
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class InputActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.input)
+    }
+}
--- app/src/main/java/com/example/linked_listxor/MainActivity.kt
+++ app/src/main/java/com/example/linked_listxor/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.linked_listxor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.linked_listxor.R
+import com.example.linked_listxor.InputActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btn1: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn1 = findViewById(R.id.comenzar)
+        btn1.setOnClickListener {
+            startActivity(Intent(this, InputActivity::class.java))
+        }
     }
 }
